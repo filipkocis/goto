2433d803d0e6ff865bab7c1ce1ee32b1931dc1d4
Filip Kocis <78610976+filipkocis@users.noreply.github.com>
2024-04-14 16:56:04 +0200

    feat(v2): bypass script sourcing with echo

--- goto.sh
+++ goto.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-VERSION="1.0.0-bash"
+VERSION="2.0.0-bash"
 AUTHOR="Filip.llc"
 
-GOTO_SAVED_PATHS=~/.config/goto/paths/
+GOTO_SAVED_PATHS=~/.config/goto/paths
 
 if [ ! -d GOTO_SAVED_PATHS ]; then
   mkdir -p $GOTO_SAVED_PATHS
@@ -11,8 +11,7 @@ fi
 
 APP_NAME="goto"
 
-# functions have gts (goto script) prefix to avoid name collisions
-gts_print_help() {
+print_help() {
   echo Version: $VERSION
   echo Author: $AUTHOR
   echo "This script is used to go to a saved location"
@@ -24,90 +23,93 @@ gts_print_help() {
   echo "$APP_NAME -l               # list all saved directories"
   echo "$APP_NAME -h               # show help"
   echo "$APP_NAME -v               # show version"
-  return 0
+  exit 0
 }
-gts_print_version() {
+print_version() {
   echo $VERSION 
-  return 0
+  exit 0
 }
-gts_save() {
+save() {
   shift
   if [ -z $1 ] ; then
     echo "Specify a name for the current directory"
     echo "Usage: $APP_NAME -s <name>"
-    return 1
+    exit 1
   fi
   local name=$(pwd)
   echo saving \"$name\" as \"$1\"
   ln -s $name $GOTO_SAVED_PATHS/$1
-  return 0
+  exit 0
 }
-gts_delete() {
+delete() {
   shift
   if [ -z $1 ] ; then
     echo "Specify a directory name to delete"
     echo "Usage: $APP_NAME -d <name>"
-    return 1
+    exit 1
   fi
   if [ ! -d $GOTO_SAVED_PATHS/$1 ] ; then
     echo "Directory \"$1\" does not exist"
-    return 1
+    exit 1
   fi
   echo removing \"$1\"
   rm $GOTO_SAVED_PATHS/$1
-  return 0
+  exit 0
 }
-gts_go_to() {
+go_to() {
   local path=$(/bin/ls -1 $GOTO_SAVED_PATHS | fzf)
   if [ -z $path ] ; then
-    return 0
+    exit 0
   fi
-  cd -P $GOTO_SAVED_PATHS/$path
-  return 0
+  echo "path"
+  echo $GOTO_SAVED_PATHS/$path
+  exit 0
 }
-gts_list_saved() {
+list_saved() {
   ls -1 $GOTO_SAVED_PATHS | while read line ; do
     echo $line
   done
-  return 0
+  exit 0
 }
 
 # goto
 if [ -z $1 ] ; then 
-  gts_go_to
-  return 0
+  go_to
+  exit 0
 fi
 
 # goto <name>
-# if directory exists, match it with grep and cd to it
+# if directory exists, match it with grep and echo the value
 if [[ ! $1 == -* ]] ; then
   count=$(/bin/ls -1 $GOTO_SAVED_PATHS | grep $1 | wc -l)
 
   if [ $count -eq 0 ] ; then
     echo "Directory \"$1\" not found"
-    return 1
+    exit 1
   fi
   
   if [ -e $GOTO_SAVED_PATHS/$1 ] ; then
-    cd -P $GOTO_SAVED_PATHS/$1
-    return 0
+    echo "path"
+    echo $GOTO_SAVED_PATHS/$1
+    exit 0
   elif [ $count -gt 1 ] ; then
     echo "Multiple directories found:"
     ls -1 $GOTO_SAVED_PATHS | grep $1
-    return 1
+    exit 1
   fi
 
-  cd -P $GOTO_SAVED_PATHS/$(/bin/ls -1 $GOTO_SAVED_PATHS | grep $1)  
-  return 0
+  echo "path"
+  echo $GOTO_SAVED_PATHS/$(/bin/ls -1 $GOTO_SAVED_PATHS | grep $1)  
+  exit 0
 fi
 
 # goto -X <name>
 option=$1
 case $option in
-  -h) gts_print_help      ;;
-  -v) gts_print_version   ;;
-  -l) gts_list_saved      ;;
-  -s) gts_save "$@"       ;;
-  -d) gts_delete "$@"     ;;
-  *) gts_print_help       ;;
+  -h) print_help      ;;
+  -v) print_version   ;;
+  -l) list_saved      ;;
+  -s) save "$@"       ;;
+  -d) delete "$@"     ;;
+  *) print_help       ;;
 esac
